71958e048ff57a9902b236a6dd7de3223250372a
Fon Nkwenti <fonnkwenti85@gmail.com>
2024-10-27 15:26:26 +0400

    clean up

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,12 @@
-output "cmd" { 
-    description = "The AWS cli command to connect to your EC2 instance through the connect point"
-    value = "aws ec2-instance-connect ssh --instance-id ${aws_instance.private_instance.id} --os-user ec2-user --connection-type eice"
-}
 
 output "cli_cmd" { 
     description = "The AWS cli command to connect to your EC2 instance through the connect point"
     value = "aws ec2-instance-connect ssh --instance-id ${module.ec2_instance.id} --os-user ec2-user --connection-type eice"
 }
 
-output "instance-id" {
+output "instance_id" {
   value = module.ec2_instance.id
+}
+output "instance_private_ip" {
+  value = module.ec2_instance.private_ip
 }
